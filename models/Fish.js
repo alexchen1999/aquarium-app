1220084ae6945f0719c995e1b3073d16ee2b1417
Alex Chen <aecchen@sas.upenn.edu>
2019-05-16 13:58:24 -0400

    created model for fish

---
+++ models/Fish.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var fishSchema = new Schema({
+    commonName: {type: String, required: true},
+    scientificName: {type: String, required: true},
+    length: Number, //in inches
+    tempLowerBound: Number, //Fahrenheit
+    tempUpperBound: Number,
+    pHLowerBound: Number,
+    pHUpperBound: Number
+});
+
+module.exports = mongoose.model('Fish', fishSchema);
+
